aa363fbcb23b1cb2be8d2995b076a4a51b6299d2
Shreyansh <shreyansh.hurkat70@gmail.com>
2022-07-08 14:26:26 +0530

    Deleted Files and added components

---
+++ src/components/Banner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <h1 class="heading-title"> SEXTANT </h1>
+  )
+}
+
+export default Banner
---
+++ src/components/Exhibit.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Exhibit() {
+  return (
+    <h3>Exhibit Component</h3>
+  )
+}
+
+export default Exhibit
